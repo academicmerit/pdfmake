fb782d13dd5943c6565b5a5225b476a21bc1fe26
Alex Noel <alex@academicmerit.com>
2018-05-01 12:22:00 +0300

    downscaling the svg after initial upscaling in canvas in ft-pdf (keeps the x2 quality)

--- build/pdfmake.js
+++ build/pdfmake.js
@@ -50097,6 +50097,15 @@ function ImageMeasure(pdfKitDoc, imageDictionary) {
 ImageMeasure.prototype.measureImage = function (src) {
 	var image, label;
 	var that = this;
+	/*
+	 * To keep the svg quality we convert it to jpeg
+	 * with dimension multiplied by 2.
+	 * And now we need to downscale it to keep the proper size.
+	 **/
+	var svgDownscaleRate = 1;
+	if (src.indexOf('__blob:') >= 0) {
+		svgDownscaleRate = 2;
+	}
 	var scale = .7 // related to issue #328
 
 	if (!this.pdfKitDoc._imageRegistry[src]) {
@@ -50111,8 +50120,8 @@ ImageMeasure.prototype.measureImage = function (src) {
 		}
 		image.embed(this.pdfKitDoc);
 
-		image.width = image.width * scale
-		image.height = image.height * scale
+		image.width = (image.width / svgDownscaleRate) * scale
+		image.height = (image.height / svgDownscaleRate) * scale
 
 		this.pdfKitDoc._imageRegistry[src] = image;
 	} else {
